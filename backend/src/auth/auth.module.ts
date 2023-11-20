f159c885f844303444f8546003e199fea18f3785
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-11-20 18:25:43 +0100

    Auth API credentials

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
+import { FortyTwoStrategy } from "./utils/FortyTwoStrategy";
 
 @Module({
 	controllers: [AuthController],
-	providers: [],
+	providers: [FortyTwoStrategy],
 })
 export class AuthModule{}
