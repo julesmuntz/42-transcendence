8c7ea0be90ec410afaa0ce375e51b896b0a25213
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-11-20 18:25:43 +0100

    Auth API credentials

--- backend/src/auth/utils/FortyTwoStrategy.ts
+++ backend/src/auth/utils/FortyTwoStrategy.ts
@@ -1,6 +1,21 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Profile } from 'passport';
 import Strategy from 'passport-42';
 
+@Injectable()
 export class FortyTwoStrategy extends PassportStrategy(Strategy) {
-
+	constructor() {
+		super({
+			clientID: API_ID,
+			clientSecret: API_SECRET,
+			callbackURL: API_CALLBACK,
+			scope: ['public'],
+		});
+	}
+	async validate(accessToken: string, refreshToken: string, profile: Profile) {
+		console.log(accessToken);
+		console.log(refreshToken);
+		console.log(profile);
+	}
 }
