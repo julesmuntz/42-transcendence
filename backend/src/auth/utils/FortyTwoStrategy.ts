121e808de3885b103ef0868ceccd4812680d3ee3
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-11-20 16:11:49 +0100

    Auth dependencies fixed

--- backend/src/auth/utils/FortyTwoStrategy.ts
+++ backend/src/auth/utils/FortyTwoStrategy.ts
@@ -1,5 +1,5 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-42';
+import Strategy from 'passport-42';
 
 export class FortyTwoStrategy extends PassportStrategy(Strategy) {
 
