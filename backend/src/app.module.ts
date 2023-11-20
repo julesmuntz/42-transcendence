be2a464472abe20fb8e079200b8a5d78fbdb6aeb
ThzEdouard <edouard.flaquet@gmail.com>
2023-11-20 18:12:35 +0100

    modif projet docker and add users controleur service module add config .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entity/user.entity';
-import { Relation } from './user/entity/relation.entity';
-import { MessageDirect } from './user/entity/message_direct.entity';
-import { Match } from './user/entity/match.entity';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-	type: 'postgres',
-    host: 'database',
-    port: 5432,
-    username: 'user-name',
-    password: 'strong-password',
-    database: 'postgres',
-    entities: [User, Relation, MessageDirect, Match],
+  imports: [ConfigModule.forRoot(),
+	UsersModule,
+	TypeOrmModule.forRoot({
+	type: process.env.TYPE as any, 
+    host: process.env.HOST,
+    port: parseInt(process.env.PORT) || 5432,
+    username: process.env.USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
   })],
   controllers: [],
