473b71ef0140bcd380b0f4cd3626f15e76686f35
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-11-21 16:45:03 +0100

    API doesn't work with DB

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { FortyTwoAuthGuard } from "./utils/Guards";
 
 @Controller('auth')
 export class AuthController{
 	@Get('login')
+	@UseGuards(FortyTwoAuthGuard)
 	getLogin() {
 		return {msg: 'test login'};
 	}
 	
 	@Get('redirect')
+	@UseGuards(FortyTwoAuthGuard)
 	getRedirect() {
 		return {msg: 'test redirect'};
 	}
