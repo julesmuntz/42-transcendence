bea82d48f7becba923d77cba1b86c3c4094e0d65
Maha BELRHAZI <mbelrhaz@paul-f4Ar5s8.clusters.42paris.fr>
2023-12-13 19:43:51 +0100

    minor change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,13 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 
 	const app = await NestFactory.create(AppModule, {cors: true});
-	// app.use(function (request: Request, response: Response, next: NextFunction) {
-	// 	response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-	// });
+
 	app.enableCors({
 		origin: true,
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 		credentials: true
 	});
+
 	await app.listen(3030);
 }
 bootstrap();
