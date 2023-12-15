6d9688baaed81fb5cb841f1f409bbcb5191a51f0
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-12-15 17:26:33 +0100

    2FA Optimized

--- frontend/src/components/LoginPage/TwoFA.tsx
+++ frontend/src/components/LoginPage/TwoFA.tsx
@@ -44,6 +44,52 @@ export default function TwoFA({ id, TFASecret }: { id: string; TFASecret: string
 		}
 	};
 
+	const handleClick = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+		e.preventDefault();
+		const input = e.target as HTMLInputElement;
+		input.value = "";
+		const codeInputNames = ["code1", "code2", "code3", "code4", "code5", "code6"];
+		
+		
+		if ((e.currentTarget.name == "code1") || (e.currentTarget.name == "code2") || (e.currentTarget.name == "code3") || (e.currentTarget.name == "code4") || (e.currentTarget.name == "code5") || (e.currentTarget.name == "code6"))
+		{
+			codeInputNames.forEach((name) => {
+				(document.getElementsByName(name)[0] as HTMLInputElement).value = "";
+			});
+			input.value = "";
+			(document.getElementsByName("code1")[0] as unknown as HTMLInputElement).focus();
+		}
+	}
+
+	const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		const codeInputNames = ["code1", "code2", "code3", "code4", "code5", "code6"];
+		if (e.key == "Tab")
+		{
+			e.preventDefault();
+			if ((e.currentTarget.name == "code1") || (e.currentTarget.name == "code2") || (e.currentTarget.name == "code3") || (e.currentTarget.name == "code4") || (e.currentTarget.name == "code5") || (e.currentTarget.name == "code6"))
+			{
+				codeInputNames.forEach((name) => {
+					(document.getElementsByName(name)[0] as HTMLInputElement).value = "";
+				});
+				(document.getElementsByName("code1")[0] as unknown as HTMLInputElement).focus();
+			}
+		}
+		if (e.key == "Backspace")
+		{
+			e.preventDefault();
+			if ((e.currentTarget.name == "code1") || (e.currentTarget.name == "code2") || (e.currentTarget.name == "code3") || (e.currentTarget.name == "code4") || (e.currentTarget.name == "code5") || (e.currentTarget.name == "code6"))
+			{
+				const input = e.target as HTMLInputElement;
+				input.value = "";
+				const prevInput = input.previousElementSibling;
+				if (prevInput !== null) {
+					(prevInput as HTMLInputElement).value = "";
+					(prevInput as HTMLInputElement).focus();
+				}
+			}
+		}
+	}
+
 	const sendCode = async (e: any) => {
 		await fetch("http://localhost:3030/2fa/authenticate", {
 			method: "POST",
@@ -91,12 +137,12 @@ export default function TwoFA({ id, TFASecret }: { id: string; TFASecret: string
 				<fieldset>
 					<TwofaLegend>Validation Code</TwofaLegend>
 					<div className="twofa-div">
-						<input name="code1" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} required />
-						<input name="code2" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} required />
-						<input name="code3" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} required />
-						<input name="code4" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} required />
-						<input name="code5" type="number" pattern="[0-1]{1}" maxLength={1} onInput={handleInput} required />
-						<input name="code6" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} required />
+						<input name="code1" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} onClick={handleClick} onKeyDown={handleKey} required />
+						<input name="code2" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} onClick={handleClick} onKeyDown={handleKey} required />
+						<input name="code3" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} onClick={handleClick} onKeyDown={handleKey} required />
+						<input name="code4" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} onClick={handleClick} onKeyDown={handleKey} required />
+						<input name="code5" type="number" pattern="[0-1]{1}" maxLength={1} onInput={handleInput} onClick={handleClick} onKeyDown={handleKey} required />
+						<input name="code6" type="number" pattern="[0-9]{1}" maxLength={1} onInput={handleInput} onClick={handleClick} onKeyDown={handleKey} required />
 					</div>
 				</fieldset>
 			</Form>
