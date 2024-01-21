4d8516e751e99dee4a6deda7319683e9724912f9
Maha BELRHAZI <mbelrhaz@bess-f4r4s11.clusters.42paris.fr>
2024-01-21 23:03:00 +0100

    update

--- frontend/src/components/Game/Game.tsx
+++ frontend/src/components/Game/Game.tsx
@@ -4,6 +4,8 @@ import Pong from './Pong';
 export default function Game() {
 	const [inGame, setInGame] = useState(false);
 
+	console.log("hello");
+
 	if (inGame)
 		return (<Pong />);
 	return (<button onClick={() => { setInGame(true); }}>YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</button>);
